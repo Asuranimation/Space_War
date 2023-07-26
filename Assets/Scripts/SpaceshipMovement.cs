522376786c613f95c5c214f84586ffc8d5f311ca
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-26 17:17:53 +0700

    Add Script SpaceshipMovement

--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class SpaceshipMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float padding;
+    private float xMin, xMax, yMin, yMax;
     void Start()
     {
-        
+        SetMovementLimit();
     }
 
     void Update()
@@ -16,8 +19,19 @@ public class SpaceshipMovement : MonoBehaviour
 
     private void Move()
     {
-        float deltaX = Input.GetAxis("Horizontal");
-        float newXPosition = transform.position.x + deltaX;
-        transform.position = new Vector2(newXPosition, transform.position.y);
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; 
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float newXPosition = Mathf.Clamp(transform.position.x + deltaX,xMin,xMax);
+        float newYPosition = Mathf.Clamp(transform.position.y + deltaY,yMin,yMax);
+        transform.position = new Vector2(newXPosition, newYPosition);
+    }
+
+    private void SetMovementLimit()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding ;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
